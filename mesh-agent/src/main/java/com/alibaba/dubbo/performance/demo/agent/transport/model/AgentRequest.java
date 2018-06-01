3f80bef7ba495017a2262312b31027e091fc91de
unknown <yjfso@qq.com>
2018-06-01 12:49:14 +0800

    清理文件

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/transport/model/AgentRequest.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/demo/agent/transport/model/AgentRequest.java
@@ -1,10 +1,6 @@
 package com.alibaba.dubbo.performance.demo.agent.transport.model;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
 
-import java.io.Serializable;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
