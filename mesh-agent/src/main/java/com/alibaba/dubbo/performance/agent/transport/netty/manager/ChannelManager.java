fa7e5512dd0687efb36ac1d0ad9082295fb440ad
unknown <yjfso@qq.com>
2018-06-08 16:59:57 +0800

    channel 多个

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/agent/transport/netty/manager/ChannelManager.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/agent/transport/netty/manager/ChannelManager.java
@@ -10,8 +10,8 @@ public class ChannelManager {
 
     private final Object lock = new Object();
     private ConnectManager connectManager;
-//    private ChannelRing channelRing = new ChannelRing();
-//    private Iterator<Channel> iterator = channelRing.iterator();
+    private ChannelRing channelRing = new ChannelRing();
+    private Iterator<Channel> iterator = channelRing.iterator();
     private Endpoint endpoint;
     private Logger logger = LoggerFactory.getLogger(ChannelManager.class);
     private Channel channel;
@@ -24,51 +24,51 @@ public class ChannelManager {
 
     void removeChannel(Channel channel){
         this.channel = null;
-//        channelRing.remove(channel);
+        channelRing.remove(channel);
 //        addNewChannel();
     }
 
-//    private void addNewChannel(){
-//        try{
-//            Channel channel = connectManager.getBootstrap().connect(endpoint.getInetSocketAddress())
-//                    .sync()
-//                    .channel();
-//            channelRing.put(channel);
-//            connectManager.registerChannel(channel, endpoint);
-//        } catch (Exception e){
-//            logger.error("new Channel error", e);
-//        }
-//
-//    }
-//
-//    private void addChannels(int num){
-//        for (Integer i = 0; i < num; i++) {
-//            addNewChannel();
-//        }
-//    }
+    private void addNewChannel(){
+        try{
+            Channel channel = connectManager.getBootstrap().connect(endpoint.getInetSocketAddress())
+                    .sync()
+                    .channel();
+            channelRing.put(channel);
+            connectManager.registerChannel(channel, endpoint);
+        } catch (Exception e){
+            logger.error("new Channel error", e);
+        }
+
+    }
+
+    private void addChannels(int num){
+        for (Integer i = 0; i < num; i++) {
+            addNewChannel();
+        }
+    }
 
     public Channel getChannel() throws Exception{
-        if (channel == null){
-            synchronized (lock){
-                if(channel == null){
-                    channel = connectManager.getBootstrap().connect(endpoint.getInetSocketAddress())
-                            .sync()
-                            .channel();
-                    connectManager.registerChannel(channel, endpoint);
-                }
-            }
-        }
-        return channel;
-//        if(!iterator.hasNext()){
+//        if (channel == null){
 //            synchronized (lock){
-//                if (!iterator.hasNext()){
-//                    addChannels(endpoint.channelNum);
+//                if(channel == null){
+//                    channel = connectManager.getBootstrap().connect(endpoint.getInetSocketAddress())
+//                            .sync()
+//                            .channel();
+//                    connectManager.registerChannel(channel, endpoint);
 //                }
 //            }
-//            if(!iterator.hasNext()){
-//                return null;
-//            }
 //        }
-//        return iterator.next();
+//        return channel;
+        if(!iterator.hasNext()){
+            synchronized (lock){
+                if (!iterator.hasNext()){
+                    addChannels(endpoint.channelNum);
+                }
+            }
+            if(!iterator.hasNext()){
+                return null;
+            }
+        }
+        return iterator.next();
     }
 }
