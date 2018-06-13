fc2684dc93431a3ae505a95fe429126c2bb728f5
yinjianfeng <yjfso@qq.com>
2018-06-13 22:54:43 +0800

    减小consumer线程池

--- mesh-agent/src/main/java/com/alibaba/dubbo/performance/agent/launcher/consumer/ConsumerHandler.java
+++ mesh-agent/src/main/java/com/alibaba/dubbo/performance/agent/launcher/consumer/ConsumerHandler.java
@@ -2,20 +2,12 @@ package com.alibaba.dubbo.performance.agent.launcher.consumer;
 
 import com.alibaba.dubbo.performance.agent.model.AgentRequest;
 
-import com.alibaba.dubbo.performance.agent.transport.netty.http.HttpUtils;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.*;
 
-
-import io.netty.util.concurrent.ThreadPerTaskExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.OK;
 import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
@@ -32,31 +24,23 @@ public class ConsumerHandler extends ChannelInboundHandlerAdapter {
     public void channelRead(ChannelHandlerContext ctx, Object msg)
             throws Exception {
 
-//        consumer.executorService.submit(()->{
-//            try{
-                if (msg instanceof FullHttpRequest) {
-                    FullHttpRequest req = (FullHttpRequest) msg;
-                    HttpMethod httpMethod = req.method();
-                    if (HttpMethod.POST.equals(httpMethod) ) {
-                        boolean keepAlive = HttpUtil.isKeepAlive(req);
-                        AgentRequest agentRequest = AgentRequest.getAgentRequest(); //AgentRequest.fromMap(paramters);
-                        agentRequest.setByteBufHolder(req);
-                        agentRequest.setCtx(ctx);
-                        agentRequest.setKeepAlive(keepAlive);
-                        try{
-                            AgentClient.INSTANCE.invoke(agentRequest);
-                        } catch (Exception e){
-                            agentRequest.done(new DefaultFullHttpResponse(HTTP_1_1, OK));
-                            log.error("agentClient invoke error", e);
-                        }
+            if (msg instanceof FullHttpRequest) {
+                FullHttpRequest req = (FullHttpRequest) msg;
+                HttpMethod httpMethod = req.method();
+                if (HttpMethod.POST.equals(httpMethod) ) {
+                    boolean keepAlive = HttpUtil.isKeepAlive(req);
+                    AgentRequest agentRequest = AgentRequest.getAgentRequest(); //AgentRequest.fromMap(paramters);
+                    agentRequest.setByteBufHolder(req);
+                    agentRequest.setCtx(ctx);
+                    agentRequest.setKeepAlive(keepAlive);
+                    try{
+                        AgentClient.INSTANCE.invoke(agentRequest);
+                    } catch (Exception e){
+                        agentRequest.done(new DefaultFullHttpResponse(HTTP_1_1, OK));
+                        log.error("agentClient invoke error", e);
                     }
                 }
-//            } catch (Exception e){
-//                log.error("consumer server catch error", e);
-//            } finally {
-////                ReferenceCountUtil.release(msg);
-//            }
-//        });
+            }
     }
 
     @Override
